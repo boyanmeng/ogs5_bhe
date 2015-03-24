f38d356bb39d19c5713e7a7c35b95d437f1afbf7
unknown <zheng@ENVINF127L.intranet.ufz.de>
2015-03-24 16:30:39 +0100

    Add the output with ice volume fraction

--- FEM/fem_ele_std.cpp
+++ FEM/fem_ele_std.cpp
@@ -2536,7 +2536,7 @@ void CFiniteElementStd::CalCoefLaplace(bool Gravity, int ip)
             // get the volume fraction of ice
             phi_i = MediaProp->CalcIceVolFrac(TG, sigmoid_coeff);
 			// output the element value
-			this->pcs->SetElementValue(this->MeshElement->GetIndex(),pcs->GetElementValueIndex("PHI_I"),phi_i);
+			this->pcs->SetElementValue(this->MeshElement->GetIndex(), pcs->GetElementValueIndex("PHI_I"), phi_i);
 
             poro = MediaProp->Porosity(Index, pcs->m_num->ls_theta);
             mat_fac = lambda_solid*(1 - poro) + lambda_ice*phi_i + lambda_water*(poro - phi_i);
