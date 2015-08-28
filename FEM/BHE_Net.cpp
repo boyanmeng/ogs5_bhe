160a1c881342ecc385deea609fd146998619811a
Haibing Shao <haibing.shao@gmail.com>
2015-08-28 12:28:11 +0200

    add the BHE_Net class.

---
+++ FEM/BHE_Net.cpp
@@ -0,0 +1,8 @@
+#include "BHE_Net.h"
+
+using namespace BHE; 
+
+BHE_Net::BHE_Net()
+{
+
+}
---
+++ FEM/BHE_Net.h
@@ -0,0 +1,28 @@
+/**
+* \file BHE_Net.h
+* 2015/08/28 HS inital implementation
+* borehole heat exchanger network class
+*
+*
+*/
+
+#ifndef BHE_NET_H
+#define BHE_NET_H
+
+
+namespace BHE  // namespace of borehole heat exchanger
+{
+    class BHE_Net {
+    public:
+        /**
+          * constructor
+          */
+        BHE_Net(); 
+
+    private:
+
+
+    };
+}
+
+#endif
