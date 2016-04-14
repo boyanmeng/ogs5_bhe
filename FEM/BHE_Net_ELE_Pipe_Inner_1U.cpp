fc9033b47ac241df40c215952c6a096058b41b3c
Haibing Shao haibing <haibing>
2016-04-14 08:39:32 +0200

    add a new class, which is designed for the pipeline connecting the bottom of 1U type BHE.

---
+++ FEM/BHE_Net_ELE_Pipe_Inner_1U.cpp
@@ -0,0 +1,14 @@
+#include "BHE_Net_ELE_Pipe_Inner_1U.h"
+
+using namespace BHE;
+
+BHE_Net_ELE_Pipe_Inner_1U::BHE_Net_ELE_Pipe_Inner_1U(std::string & name)
+    : BHE_Net_ELE_Abstract(name, BHE_NET_ELE::BHE_NET_PIPE_INNER_1U, 1, 1)
+{
+
+}
+
+double BHE_Net_ELE_Pipe_Inner_1U::get_RHS_value()
+{
+    return 0;
+}
---
+++ FEM/BHE_Net_ELE_Pipe_Inner_1U.h
@@ -0,0 +1,31 @@
+/**
+* \file BHE_Net_ELE_Pipe_Inner_1U.h
+* 2016/04/14 HS inital implementation
+* borehole heat exchanger network element pipeline class
+* particularly designed for 1U type of BHE
+* used to connect the bottom of BHE, linking the inlet and 
+* outlet pipelines
+*/
+
+#ifndef BHE_NET_ELE_PIPE_INNER_1U_H
+#define BHE_NET_ELE_PIPE_INNER_1U_H
+
+#include "BHE_Net_ELE_Abstract.h"
+
+namespace BHE  // namespace of borehole heat exchanger
+{
+    class BHE_Net_ELE_Pipe_Inner_1U : public BHE_Net_ELE_Abstract {
+
+    public:
+        /**
+        * constructor
+        */
+        BHE_Net_ELE_Pipe_Inner_1U(std::string & name);
+
+        double get_RHS_value();
+
+    };
+
+}
+
+#endif
